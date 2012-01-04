ee7022af501638827786d8abe5410a1e3967d395
Rudi <martinr@Notebook-10036.local>
2012-01-04 15:46:39 +0100

    transparent proxy

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+
+http.createServer(function(request, response) {
+  var proxy = http.createClient(3128, "duplex.hsh-berlin.com")
+  var proxy_request = proxy.request(request.method, request.url, request.headers);
+  proxy_request.addListener('response', function (proxy_response) {
+    proxy_response.addListener('data', function(chunk) {
+    	console.log(chunk);
+      response.write(chunk, 'binary');
+    });
+    proxy_response.addListener('end', function() {
+      response.end();
+    });
+    response.writeHead(proxy_response.statusCode, proxy_response.headers);
+  });
+  request.addListener('data', function(chunk) {
+  	console.log(chunk);
+    proxy_request.write(chunk, 'binary');
+  });
+  request.addListener('end', function() {
+    proxy_request.end();
+  });
+}).listen(8080);
